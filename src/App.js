871cd754e97773294c17eda617c69a993f5bac12
Môrisson Marcel <morissonmaciel@hotmail.com>
2024-02-26 20:12:13 -0300

    -- adding styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { RootContainer } from "./App.styled";
 
 function App() {
   return (
-    <div>
+    <RootContainer>
       <h1>Hello, World!</h1>
-    </div>
+    </RootContainer>
   );
 }
 
