192a81e9120f1be0af1986300bf44309fba5b261
Môrisson Marcel <morissonmaciel@hotmail.com>
2024-02-26 20:23:07 -0300

    -- adding image to render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { RootContainer } from "./App.styled";
+import reactLogo from "./images/react-logo.svg";
 
 function App() {
   return (
     <RootContainer>
       <h1>Hello, World!</h1>
+      <img src={reactLogo} alt="" width={128} height={128} />
     </RootContainer>
   );
 }
